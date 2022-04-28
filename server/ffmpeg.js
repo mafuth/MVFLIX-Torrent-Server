800c24c310d2ca3f77ddf25e83fe8606f52471da
Mohamed Mafaaz <mafaaz708@gmail.com>
2022-04-28 15:44:43 +0500

    ffmpeg will now convert to mp4

--- server/ffmpeg.js
+++ server/ffmpeg.js
@@ -8,12 +8,13 @@ module.exports = function (req, res, torrent, file) {
   var param = req.query.ffmpeg,
     ffmpeg = require('fluent-ffmpeg');
 
-  res.type('video/webm');
+  res.type('video/mp4');
   var command = ffmpeg(file.createReadStream())
-    .videoCodec('libvpx').audioCodec('libvorbis').format('webm')
-    .audioBitrate(128)
-    .videoBitrate(1024)
     .outputOptions([
+      '-c:v libx264',
+      '-g 52',
+      '-movflags frag_keyframe+empty_moov',
+      '-f mp4',
       '-threads 20',
       '-deadline realtime',
       '-error-resilient 1'
