95ee8fadf69cfd7afedf433e5375e048c15777c6
Mohamed Mafaaz <57235839+mafuth@users.noreply.github.com>
2022-05-14 19:42:42 +0500

    Update main.js

--- html/scripts/controllers/main.js
+++ html/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* global Push */
-angular.module('MVFlix-torrent-server-app')
+angular.module('peerflixServerApp')
   .controller('MainCtrl', function ($scope, $resource, $log, $q, $upload, torrentSocket) {
     var Torrent = $resource('/torrents/:infoHash');
 
