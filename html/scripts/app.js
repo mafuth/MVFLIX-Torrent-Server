f2897d08ea70c87ef63b163a2b4c4a8bc16b9458
Mohamed Mafaaz <57235839+mafuth@users.noreply.github.com>
2022-05-14 19:50:11 +0500

    Update app.js

--- html/scripts/app.js
+++ html/scripts/app.js
@@ -12,7 +12,7 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/app.html',
+        templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
       .otherwise({
